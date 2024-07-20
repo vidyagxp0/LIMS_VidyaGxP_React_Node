8d4234005a985f77dd89b83582a6a22edc780378
Anshul <139344452+Mranshul1997@users.noreply.github.com>
2024-07-20 12:48:05 +0530

    Add (import & Expor)t functionality in all Grids

--- Frontend/src/Pages/Modals/importModal.jsx
+++ Frontend/src/Pages/Modals/importModal.jsx
@@ -1,8 +1,14 @@
-import React, { useRef, useState } from 'react';
-import * as XLSX from 'xlsx';
-import { toast } from 'react-toastify';
+import React, { useRef, useState } from "react";
+import * as XLSX from "xlsx";
+import { toast } from "react-toastify";
 
-const ImportModal = ({ isOpen, onClose, initialData, columns, onDataUpload }) => {
+const ImportModal = ({
+  isOpen,
+  onClose,
+  initialData,
+  columns,
+  onDataUpload,
+}) => {
   const fileInputRef = useRef(null);
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
 
@@ -10,27 +16,29 @@ const ImportModal = ({ isOpen, onClose, initialData, columns, onDataUpload }) =>
 
   const handleCreateExcel = () => {
     const wb = XLSX.utils.book_new();
-    const headerRow = columns.map(column => column.header);
-    const dataRows = initialData.map(data => columns.map(column => data[column.accessor]));
+    const headerRow = columns.map((column) => column.header);
+    const dataRows = initialData.map((data) =>
+      columns.map((column) => data[column.accessor])
+    );
 
     const ws = XLSX.utils.aoa_to_sheet([headerRow, ...dataRows]);
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, 'sample.xlsx');
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+    XLSX.writeFile(wb, "sample.xlsx");
   };
 
-  const handleFileUpload = event => {
+  const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = e => {
+    reader.onload = (e) => {
       const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: 'array' });
+      const workbook = XLSX.read(data, { type: "array" });
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
       onDataUpload(jsonData);
-      toast.success('File uploaded successfully!');
+      toast.success("File uploaded successfully!");
       setIsSuccessModalOpen(true);
       console.log(jsonData);
     };
@@ -54,8 +62,12 @@ const ImportModal = ({ isOpen, onClose, initialData, columns, onDataUpload }) =>
           <h2 className="text-xl font-semibold mb-4">Bulk Upload Data</h2>
           <p className="mb-4">Upload your data through CSV or XLS file.</p>
           <div className="mb-4">
-            <h3 className="text-lg font-semibold">Step 1: Download Sample Template</h3>
-            <p className="mb-2">Download sample template by clicking the button below.</p>
+            <h3 className="text-lg font-semibold">
+              Step 1: Download Sample Template
+            </h3>
+            <p className="mb-2">
+              Download sample template by clicking the button below.
+            </p>
             <button
               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
               onClick={handleCreateExcel}
@@ -65,7 +77,9 @@ const ImportModal = ({ isOpen, onClose, initialData, columns, onDataUpload }) =>
           </div>
           <div className="mb-4">
             <h3 className="text-lg font-semibold">Step 2: Upload CSV/XLS</h3>
-            <p className="mb-2">Upload the edited template by clicking the button below.</p>
+            <p className="mb-2">
+              Upload the edited template by clicking the button below.
+            </p>
             <button
               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
               onClick={handleUploadButtonClick}
@@ -75,7 +89,7 @@ const ImportModal = ({ isOpen, onClose, initialData, columns, onDataUpload }) =>
             <input
               type="file"
               ref={fileInputRef}
-              style={{ display: 'none' }}
+              style={{ display: "none" }}
               accept=".csv, .xlsx, .xls"
               onChange={handleFileUpload}
             />
